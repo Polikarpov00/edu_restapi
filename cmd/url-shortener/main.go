e6641ef339513d52df49739d6dacbf9bf66a31f7
Gehrman Polikarpov <polikarpov@MacBook-Air-Gehrman.local>
2025-06-13 18:34:06 +0300

    added logger and db connection

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"restapi/internal/config"
+	"restapi/internal/lib/logger/sl"
+	"restapi/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	// fmt.Println(cfg)
+
+	log.Info("url-shortener starting", slog.String("env", cfg.Env))
+	log.Debug("Debug is enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+	switch env {
+
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
