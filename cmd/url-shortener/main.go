81fcb2b947398bae5c930350486cd585c7d42ab2
Gehrman Polikarpov <polikarpov@MacBook-Air-Gehrman.local>
2025-06-15 18:51:33 +0300

    added saveUrl function

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -29,7 +29,22 @@ func main() {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
-	_ = storage
+
+	// id, err := storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+
+	// log.Info("saved url", slog.Int64("id", id))
+
+	// id, err = storage.SaveURL("https://google.com", "google")
+	// if err != nil {
+	// 	log.Error("failed to save url", sl.Err(err))
+	// 	os.Exit(1)
+	// }
+
+	// _ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
